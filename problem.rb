58eb2305a19368421a211b2cbd7e41c4bfb3b6ff
Rahim Packir Saibo <i.am@rah.im>
2023-09-19 16:51:29 +0100

    Attempting to reproduce: match gem versions

--- problem.rb
+++ problem.rb
@@ -4,7 +4,12 @@ require 'bundler/inline'
 
 gemfile do
   source 'https://rubygems.org'
-  gem 'fog-aws', '= 3.19.0'
+  # gem 'fog-aws', '= 3.19.0'
+  gem 'fog-aws', '= 1.4.0'
+  gem 'fog-core', '= 1.45.0'
+  gem 'fog-xml', '= 0.1.3'
+  gem 'mime-types'
+  gem 'pry-byebug'
 end
 
 config = {
@@ -15,6 +20,30 @@ config = {
 }
 
 s3 = Fog::Storage.new(config)
-directory = s3.directories.new(key: 'litmus-dev-rahim-throwaway')
-directory.files.get('foo')
-file = directory.files.create(key: 'foo', body: 'bar')
+bucket = s3.directories.new(key: 'litmus-dev-rahim-throwaway')
+binding.pry
+bucket.files.get('foo')
+# =>   <Fog::AWS::Storage::File
+#     key="foo",
+#     cache_control=nil,
+#     content_disposition=nil,
+#     content_encoding=nil,
+#     content_length=3,
+#     content_md5=nil,
+#     content_type="",
+#     etag="37b51d194a7513e45b56f6524f2d51f2",
+#     expires=nil,
+#     last_modified=2023-09-19 15:18:30 +0000,
+#     metadata={"x-amz-id-2"=>"P6b145GU6tllNufELCAYtGGNIvqijkuVoayaabSTGi3d4dVxzZCM5TYiaCgZhvOpmJO/SKTHyOnpQw65oXKXug==", "x-amz-request-id"=>"PAYX90KF2VFZMBVH"},
+#     owner=nil,
+#     storage_class=nil,
+#     encryption="AES256",
+#     encryption_key=nil,
+#     version=nil,
+#     kms_key_id=nil,
+#     tags=nil,
+#     website_redirect_location=nil
+#   >
+file = bucket.files.create(key: 'foo', body: 'bar')
+bucket.files.get('foo')
+file = bucket.files.create(key: 'foo', body: 'bar')
